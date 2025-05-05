ebbfbee25a7aabf5adf8b03e877d174b3f1db756
Your Name <you@example.com>
2025-05-05 21:42:59 +0300

    changes

--- personal/settings_pro.py
+++ personal/settings_pro.py
@@ -1,9 +1,11 @@
 from .settings import *
+from decouple import config
+import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['personal-vhgz.onrender.com']
 
 
 # Application definition
@@ -21,15 +23,18 @@ INSTALLED_APPS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+     #'default': {
+         #'ENGINE': 'django.db.backends.postgresql',
+         #'NAME': config('DB_NAME'),
+         #'USER': config('DB_USER'),
+         #'PASSWORD': config('DB_PASSWORD'),
+         #'HOST': config('DB_HOST', default='localhost'),
+     #}
+ #}
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': config('DB_NAME'),
-         'USER': config('DB_USER'),
-         'PASSWORD': config('DB_PASSWORD'),
-         'HOST': config('DB_HOST', default='localhost'),
-     }
- }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
+}
 
 # HTTPS settings
 #SESSION_COOKIE_SECURE = True
