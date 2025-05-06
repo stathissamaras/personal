4d31fee1828165a09c6ebeaba1440abd39834c48
Your Name <you@example.com>
2025-05-06 10:56:55 +0300

    changes

--- personal/settings.py
+++ personal/settings.py
@@ -137,12 +137,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # <-- πηγή
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- προορισμός
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- personal/settings_pro.py
+++ personal/settings_pro.py
@@ -60,12 +60,12 @@ DEFAULT_FROM_EMAIL = 'info@oileco.gr'
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')  # <-- πηγή
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- προορισμός
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
